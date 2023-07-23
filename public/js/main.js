e3fca2af1c7b3f8ad1d6da771c488444a2e44993
webnetkz <dbqqbq@gmail.com>
2023-07-23 19:23:05 +0600

    Fix mobile styles

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () =>
+{
+    const classes = ['layer-1', 'layer-2', 'layer-5', 'layer-6'];
+    const images = [
+        './public/img/layer-1.webp',
+        './public/img/layer-2.webp',
+        './public/img/layer-5.webp',
+        './public/img/layer-6.webp'
+    ];
+
+    classes.forEach((className, index) =>
+    {
+        const elements = document.getElementsByClassName(className);
+        for(let i = 0; i < elements.length; i++)
+        {
+            elements[i].style.backgroundImage = `url(${images[index]})`;
+        }
+    });
+});
