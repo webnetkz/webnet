9392701b51a457cebb6d46e8dfd3a6a6b990fa83
webnetkz <dbqqbq@gmail.com>
2023-07-23 19:54:05 +0600

    Append parallax for mobile

--- public/js/3d.js
+++ public/js/3d.js
@@ -7,4 +7,29 @@ document.addEventListener('mousemove', e =>
 			--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
 		`
 	})
+
+	/**
+	 * Mobile
+	 */
+
+    if((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1))
+    {
+
+		window.addEventListener("deviceorientation", function(event)
+		{
+
+			let beta = (Math.round(event.beta) / 180) * 6;
+			let gamma = (Math.round(event.gamma) / 90) * 3;
+
+			Object.assign(document.documentElement, 
+			{
+				style: `
+					--move-x: ${(beta - window.innerWidth / 2) * -.005}deg;
+					--move-y: ${(gamma - window.innerHeight / 2) * .01}deg;
+				`
+			})
+		}, true);
+
+
+    }
 })
