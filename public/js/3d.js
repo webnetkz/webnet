630f9f7f1911b81352883bb2792d594bef55b737
webnetkz <dbqqbq@gmail.com>
2023-07-26 10:36:44 +0600

    Append google analytics, append SEO tag for google

--- public/js/3d.js
+++ public/js/3d.js
@@ -7,27 +7,4 @@ document.addEventListener('mousemove', e =>
 			--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
 		`
 	})
-
-	/**
-	 * Mobile
-	 */
-
-    if((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1))
-    {
-
-		window.addEventListener("deviceorientation", function(event)
-		{
-		  let container = document.querySelector('section.layers');
-		  let beta = (Math.round(event.beta) / 180) * 6;
-		  let gamma = (Math.round(event.gamma) / 90) * 3;
-  
-		  const maxRotation = 30;
-		  beta *= maxRotation;
-		  gamma *= maxRotation;
-  
-		  container.style.transform = 'rotateX(' + -beta + 'deg) rotateY(' + gamma + 'deg)';
-		}, true);
-
-
-    }
 })
