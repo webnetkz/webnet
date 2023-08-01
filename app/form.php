17f85df1807a505d32baee974c0ffcc6c0353ed5
webnetkz <dbqqbq@gmail.com>
2023-08-01 16:01:52 +0600

    Create telegram feedback

--- app/form.php
+++ app/form.php
@@ -1,4 +1,21 @@
 <?php
 
-echo '<pre>';
-var_dump($_POST);
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $name = $_POST["name"];
+    $phone = $_POST["phone"];
+
+    $subject = 'Запрос с формы на сайте';
+
+    $to = 'info@webnet.kz';
+
+    $headers = "From: webnet@webnet.kz\r\n";
+    $headers .= "Reply-To: dbqqbq@gmail.com\r\n"; 
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    if (mail($to, $subject, $message, $headers)) {
+        header('Location: ../index.html');
+    } else {
+		header('Location: ../index.html');
+    }
+}
---
+++ public/js/send_form.js
@@ -0,0 +1,32 @@
+document.querySelector("#feedback form").addEventListener("submit", function(event)
+{
+    event.preventDefault();
+
+    let name = document.querySelector("input[name='name']").value;
+    let phone = document.querySelector("input[name='phone']").value;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "./app/send_message_tel.php", true);
+
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhr.onload = function()
+    {
+        if (xhr.status === 200)
+        {
+            console.log(xhr.responseText); // Вывод ответа в консоль (для отладки)
+        }
+        else
+        {
+            console.error("Ошибка " + xhr.status + ": " + xhr.statusText);
+        }
+    };
+
+    xhr.onerror = function()
+    {
+        console.error("Ошибка сети");
+    };
+
+    xhr.send("name=" + encodeURIComponent(name) + "&phone=" + encodeURIComponent(phone));
+});
