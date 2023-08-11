69b2ac33ca9ba8eb27855bbf4ceb9ed56294b20a
webnetkz <dbqqbq@gmail.com>
2023-08-11 19:31:15 +0600

    Create 3d component

---
+++ components/3d.php
@@ -0,0 +1,18 @@
+<section class="layers">
+    <div class="layers-container">
+        <div class="layers-item layer-1""></div>
+        <div class="layers-item layer-2""></div>
+        <div class="layers-item layer-3">
+            <div class="hero-content">
+                <h1>Разработка<span>цифровых продуктов</span></h1>
+                <div class="hero-content-p">WEB, IOS, MacOS, Android, Windows, Linux</div>
+                <a class="button" href="tel:+77076691424">+7 707 669 14 23</a>
+            </div>
+        </div>
+        <div class="layers-item layer-4">
+            <canvas class="rain"></canvas>
+        </div>
+        <div class="layers-item layer-5"></div>
+        <div class="layers-item layer-6"></div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <?php
 
 require_once './components/header.php';
+require_once './components/3d.php';
+
 
 ?>
-	<section class="layers">
-		<div class="layers-container">
-			<div class="layers-item layer-1""></div>
-			<div class="layers-item layer-2""></div>
-			<div class="layers-item layer-3">
-				<div class="hero-content">
-					<h1>Разработка<span>цифровых продуктов</span></h1>
-					<div class="hero-content-p">WEB, IOS, MacOS, Android, Windows, Linux</div>
-					<a class="button" href="tel:+77076691424">+7 707 669 14 23</a>
-				</div>
-			</div>
-			<div class="layers-item layer-4">
-				<canvas class="rain"></canvas>
-			</div>
-			<div class="layers-item layer-5"></div>
-			<div class="layers-item layer-6"></div>
-		</div>
-	</section>
+	
 
 
     <div class="utp-section">	
