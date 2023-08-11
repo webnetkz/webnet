bf57230959ab62d67d1e5e89aa1ec64882f513a1
webnetkz <dbqqbq@gmail.com>
2023-08-11 19:26:43 +0600

    Create header component

---
+++ components/header.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once './config.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+	<meta name="description" content="WebNet разработка сайтов и приложений">
+	<link rel="icon" type="image/png" href="./public/img/icon.png" loading="lazy">
+	<link rel="manifest" href="./manifest.json">
+	<meta name="theme-color" content="#000"/>
+	<meta name="google-site-verification" content="KnCNMaptYRR7po3CwfepSJQK-YphmObbrallV8LeKlA" />
+
+    <title>WebNet</title>
+
+    <link rel="stylesheet" href="./dist/min.css">
+</head>
+<body>
+
+	<style>
+		header
+		{
+			position: fixed;
+			left: 0;
+			top: 0;
+			width: 100%;
+			height: 55px;
+			z-index: var(--z-header);
+			filter: blur(7px);
+	
+		}
+	</style>
+
+	<header>
+		<menu>
+			<p>123</p>
+			<p>312</p>
+		</menu>
+	</header>
