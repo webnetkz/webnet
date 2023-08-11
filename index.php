47cd8fa8035daa830b0c5ce962ed59a489cd59b0
webnetkz <dbqqbq@gmail.com>
2023-08-11 19:32:46 +0600

    Create upt component, equal index page

--- index.php
+++ index.php
@@ -2,58 +2,7 @@
 
 require_once './components/header.php';
 require_once './components/3d.php';
-
-
-?>
-	
-
-
-    <div class="utp-section">	
-
-		<section class="wrapper">
-			<div class="top">WebNet</div>
-			<div class="bottom" aria-hidden="true">WebNet</div>
-		</section>
-
-		<div id='stars'></div>
-		<div id='stars2'></div>
-		<div id='stars3'></div>
-		<div id='title'>
-		<span>
-			Самые высокие
-		</span>
-		<br>
-		<span>
-			мировые стандарты разработки!
-		</span>
-		</div>
-
-
-        <div class="promo">
-            <p>
-				Прохождение всех тестов от 
-				<b style="font-size: 2rem; display: inline-block;">
-					<span style="color: #4284F3;">G</span> 
-					<span style="color: #ea4436;margin: -7px;">O</span>
-					<span style="color: #FABC08;margin: -3px;">O</span>
-					<span style="color: #4284F3;margin: -3px;">G</span>
-					<span style="color: #33A853;margin: -8px;">L</span>
-					<span style="color: #ea4436;">E</span>
-				</b>
-				<br>
-				<b>SEO</b> оптимизация на <b>100%</b>
-				<br>
-				Прогрессивные WEB приложения <b>PWA</b>
-			</p>
-            <button class="button" onclick="location.href = './pages/lighthouse.php'">Подробнее</button>
-        </div>
-
-    </div>
-
-	<?php
-
-	require_once './components/form.php';
-	require_once './components/services.php';
-	require_once './components/footer.php';
-
-	?>
+require_once './components/utp.php';
+require_once './components/form.php';
+require_once './components/services.php';
+require_once './components/footer.php';
