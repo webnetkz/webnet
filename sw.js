600e90984a24b5e4bea6d52b890637a384f5af02
webnetkz <dbqqbq@gmail.com>
2023-08-28 02:33:30 +0600

    Update styles of 404 page

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('my-cache').then(function(cache) {
+    caches.open('my-cache').then(function (cache) {
       return cache.addAll([
         '/',
         '/index.php',
@@ -15,7 +15,7 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
